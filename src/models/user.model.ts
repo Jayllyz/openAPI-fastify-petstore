ec1c6960fe2953a8228854a4d65903340245c8ec
Jayllyz <antonydavid945@gmail.com>
2023-06-05 15:26:00 +0200

    add: jwt auth

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,11 +1,11 @@
 import type { User } from '@prisma/client';
 import { prisma } from '../db.server';
 export type { User } from '@prisma/client';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
-const jwt = require('jsonwebtoken');
-
-export async function generateToken(user: User) {
-  const token = jwt.sign({ user }, process.env.JWT_SECRET, { expiresIn: '1h' });
+export async function generateToken(id: User['id']) {
+  const token = jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '1h' });
   return token;
 }
 
@@ -13,11 +13,37 @@ export async function dbUserById(id: User['id']) {
   return prisma.user.findUnique({ where: { id } });
 }
 
+export async function verifyToken(token: User['token']) {
+  const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+  const user = await dbUserById(decoded.id);
+  if (!user) {
+    throw new Error('Invalid token');
+  }
+}
+
+export async function dbCreateUser(name: User['name'], email: User['email'], password: User['password']) {
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const token = null;
+  return prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+      token,
+    },
+  });
+}
+
 export async function dbLoginUser(email: User['email'], password: User['password']) {
   const user = await prisma.user.findUnique({ where: { email } });
-  if (user && user.password === password) {
+  if (user) {
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      throw new Error('Invalid credentials !');
+    }
     return user;
   }
+
   return null;
 }
 
@@ -25,6 +51,6 @@ export async function revokeToken(id: User['id']) {
   return prisma.user.update({ where: { id }, data: { token: null } });
 }
 
-export async function createToken(id: User['id'], token: User['token']) {
+export async function dbSetToken(id: User['id'], token: User['token']) {
   return prisma.user.update({ where: { id }, data: { token } });
 }
